4095b59d281724e5461f2ff15d16eac8a33237c5
maru <sai.vichet70@gmail.com>
2018-05-07 17:38:34 +0700

    make it better

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Creative - Start Bootstrap Theme</title>
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/creative.css" rel="stylesheet">
+</head>
+
+<body id="page-top">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <div class="container">
+            <img src="{{ url('images/navigation_bar_and_bottom/navigation_bar_logo_white.png') }}" width="100" height="30px" id="logo">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="/">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="/news">NEWS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="/app">APP</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="/community">COMMUNITY</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="/join-us">JOIN US</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <header class="masthead text-center text-blue d-flex">
+        <div class="container my-auto">
+            <div class="row">
+                <div class="col-lg-10 mx-auto">
+                    <h1 class="text-uppercase">
+              <img src="{{ url('img/index/banner_logo.png') }}">
+            </h1>
+                    <hr>
+                </div>
+                <div class="col-lg-8 mx-auto">
+                    <p class="text-faded mb-5">Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
+                    <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="bg-primary">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mx-auto text-center">
+                    <h2 class="section-heading text-blue">We've got what you need!</h2>
+                    <p class="text-blue mb-4">Start Bootstrap has everything you need to get your new website up and running in no time!</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-1 mx-auto">
+                </div>
+                <div class="col-lg-5 mx-auto">
+                    <p class="text-blue mb-4">Start Bootstrap has everything you need to get your new website up and running in no time!</p>
+                </div>
+                <div class="col-lg-5 mx-auto">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+                <img src="{{ url('img/index/picture_1.png') }}" class="img-fluid">
+            </div>
+    </section>
+    <section class="section_3">
+        <div class="container my-auto">
+            <div class="row">
+                <div class="col-lg-5 mx-auto section_3_img">
+                    <img src="{{ url('img/index/picture_2.png') }}" class="img-fluid" height="100px">
+                </div>
+                <div class="col-lg-5 mx-auto section_3_text">
+                    <p class="text-faded text-blue mb-5">Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section_4">
+        <div class="container my-auto">
+            <div class="row">
+                <div class="col-lg-5 mx-auto section_3_text">
+                    <p class="text-faded text-blue mb-5">Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
+                </div>
+                <div class="col-lg-5 mx-auto section_3_img">
+                    <img src="{{ url('img/index/picture_2.png') }}" class="img-fluid" height="100px">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section_5">
+        <div class="container my-auto">
+            <div class="row">
+                <div class="col-lg-5 mx-auto section_3_img">
+                    <img src="{{ url('img/index/picture_2.png') }}" class="img-fluid" height="100px">
+                </div>
+                <div class="col-lg-5 mx-auto section_3_text">
+                    <p class="text-faded text-blue mb-5">Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section_6">
+        <div class="container my-auto">
+            <div class="row">
+                <div class="col-lg-5 mx-auto text-center">
+                    <p class="text-faded text-blue mb-5">Start Bootstrap can help you build better</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3"></div>
+                <div class="col-lg-1 mx-auto text-center">
+                    <img src="{{ url('img/index/WeChat.jpg') }}" class="img-fluid" height="100px">
+                    <p class="text-faded text-blue mb-5">Wechat</p>
+                </div>
+                <div class="col-lg-1 mx-auto text-center">
+                    <img src="{{ url('img/index/fb.jpg') }}" class="img-fluid" height="100px">
+                    <p class="text-faded text-blue mb-5">Facebook</p>
+                </div>
+                <div class="col-lg-1 mx-auto text-center">
+                    <img src="{{ url('img/index/Twitter.jpg') }}" class="img-fluid" height="100px">
+                    <p class="text-faded text-blue mb-5">Twitter</p>
+                </div>
+                <div class="col-lg-1 mx-auto text-center">
+                    <img src="{{ url('img/index/telegram.jpg') }}" class="img-fluid" height="100px">
+                    <p class="text-faded text-blue mb-5">Telegram</p>
+                </div>
+                <div class="col-lg-3"></div>
+            </div>
+        </div>
+    </section>
+    
+    <footer>
+        <div class="container">
+            <div class="pull-left">
+                <div class="row">
+                    <img src="{{ url('images/navigation_bar_and_bottom/buttom_logo.png') }}" width="50px" height="25spx">
+                    <p class="text-center footer-left">© U are beWiN Team. 2018</p>
+                </div>
+            </div>
+            <div class="pull-right">
+                <div class="row footer-right">
+                    <a href="" class="lang">ch</a>
+                    <p class="lang">|</p>
+                    <a href="" class="lang">eng</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.js"></script>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('layouts.app');
+});
+
+Route::get('/news', function () {
+    return view('welcome');
+});
+
+Route::get('/join-us', function () {
+    return view('join_us');
+});
