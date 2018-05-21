aa0914c8c37a4628029cdb4e34b24dea0625a26c
maru <sai.vichet70@gmail.com>
2018-05-21 13:02:23 +0700

    community logo

--- resources/views/pages/community.blade.php
+++ resources/views/pages/community.blade.php
@@ -23,7 +23,7 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-shrink menu" id="mainNav">
         <div class="container-fluid">
-            <img src="{{ url('images/new_logo/blue.png') }}" width="50" height="30px" id="logo">
+            <img src="{{ url('images/new_logo/blue.png') }}" width="50" height="50px" id="logo">
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
